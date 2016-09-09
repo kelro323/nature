248d6bc7a8ad8cfce27721fa2d6ab34555bd45ff
Seungwoo <Seungwoo@Biz>
2016-09-09 16:38:27 +0900

    library update(arirang-morph Sep.06 Version)-minor fix

--- nature/src/main/java/com/jane191/nature/SubVerbUtil.java
+++ nature/src/main/java/com/jane191/nature/SubVerbUtil.java
@@ -25,29 +25,29 @@ public class SubVerbUtil {
 						.substring(former.get(0).getStem().length());
 				if(target.get(i).getStem().equals("보다")) {
 					if(check.equals("어")){
-						target.get(i).setPatn(50); // PosType 변경예정 라이브러리 업데이트 이후	
+						target.get(i).setPosType('b');
 					} else if(check.equals("고")||check.equals("다")||check.equals("다가")) {
 						
 					} else if(check.endsWith("은가") || check.endsWith("는가")
 							||check.endsWith("나")||check.endsWith("까")) {
-						target.get(i).setPatn(60); //보조 형용사
+						target.get(i).setPosType('b'); //보조 형용사
 					} 
 				} else if(target.get(i).getStem().equals("가다")
 						||target.get(i).getStem().equals("가지다")) {
 					if(check.endsWith("어")) {
-						target.get(i).setPatn(50);
+						target.get(i).setPosType('b');
 					}
 				} else if(target.get(i).getStem().equals("나다")) {
 					if(check.equals("어")||check.equals("고")) {
-						target.get(i).setPatn(50);
+						target.get(i).setPosType('b');
 					}
 				} else if(target.get(i).getStem().equals("내다")) {
 					if(check.equals("어")) {
-						target.get(i).setPatn(50);
+						target.get(i).setPosType('b');
 					}
 				} else if(target.get(i).getStem().equals("놓다")) {
 					if(check.equals("어")) {
-						target.get(i).setPatn(50);
+						target.get(i).setPosType('b');
 					}
 				} else if(target.get(i).getStem().equals("대다")
 						||target.get(i).getStem().equals("두다")
@@ -56,43 +56,43 @@ public class SubVerbUtil {
 						||target.get(i).getStem().equals("오다")
 						||target.get(i).getStem().equals("주다")) {
 					if(check.equals("어")||former.get(0).getEomi().endsWith("어")) {
-						target.get(i).setPatn(50);
+						target.get(i).setPosType('b');
 					}
 				} else if(target.get(i).getStem().equals("말다")) {
 					if(check.equals("지")) {
-						target.get(i).setPatn(50); //부정의 의미를 주는 보조동사라서 분리
+						target.get(i).setPosType('b'); //부정의 의미
 					} else if(check.equals("고")||check.equals("고야")) {
-						target.get(i).setPatn(50);
+						target.get(i).setPosType('b');
 					}
 				} else if(target.get(i).getStem().equals("못하다")
 						||target.get(i).getStem().equals("않다")) {
 					if(check.equals("지")) {
-						target.get(i).setPatn(50);//부정의 의미(보조동사+보조형용사)
+						target.get(i).setPosType('b');//부정의 의미(보조동사+보조형용사)
 					}
 				} else if(target.get(i).getStem().equals("쌓다")) {
 					if(check.equals("어")) {
-						target.get(i).setPatn(50);//강조의 의미
+						target.get(i).setPosType('b');//강조의 의미
 					}
 				} else if(target.get(i).getStem().equals("있다")) {
 					if(check.equals("어")||check.equals("고")) {
-						target.get(i).setPatn(50);//진행의 의미
+						target.get(i).setPosType('b');//진행의 의미
 					}
 				} else if(target.get(i).getStem().equals("하다")) {
 					if(check.equals("게")) {
-						target.get(i).setPatn(50);//명령의 의미
+						target.get(i).setPosType('b');//명령의 의미
 					} else if(check.endsWith("으면")) {
-						target.get(i).setPatn(50);//바람의 의미
+						target.get(i).setPosType('b');//바람의 의미
 					} else if(check.endsWith("야")) {
-						target.get(i).setPatn(50);//필요의 의미
+						target.get(i).setPosType('b');//필요의 의미
 					} else if(check.endsWith("려고")||check.endsWith("으려")
 							||check.endsWith("으려고")||check.endsWith("고자")) {
-						target.get(i).setPatn(50);//의도 or 바람의 의미
+						target.get(i).setPosType('b');//의도 or 바람의 의미
 					} else if(check.endsWith("기는")||check.endsWith("기도")||check.endsWith("기나")) {
-						target.get(i).setPatn(50); //강조의 의미
+						target.get(i).setPosType('b'); //강조의 의미
 					} else if(check.endsWith("고")) {
-						target.get(i).setPatn(50);//이유나 근거의 의미
+						target.get(i).setPosType('b');//이유나 근거의 의미
 					} else if(check.endsWith("고는")||check.endsWith("곤")) {
-						target.get(i).setPatn(50);//반복의 의미
+						target.get(i).setPosType('b');//반복의 의미
 					}
 				} 
 			}
