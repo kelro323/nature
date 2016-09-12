7862ffd87dadf3974ecb99ee1f1159dd53ce6ff5
Seungwoo <Seungwoo@Biz>
2016-09-12 18:22:34 +0900

    SubVerbUtil minor patch

--- nature/src/main/java/com/jane191/nature/SubVerbUtil.java
+++ nature/src/main/java/com/jane191/nature/SubVerbUtil.java
@@ -16,49 +16,56 @@ public class SubVerbUtil {
 		}
 	};
 	//의미의 차이가 없이 그냥 삭제가 가능한 경우 하나로 묶으면 될듯
+	//equals 이랑 endsWith 조건에 대해선 좀 더 검수가 필요함
 	public static void checkSubVerb(List<AnalysisOutput> target, List<AnalysisOutput> former) {
 		int size = target.size();
 		for(int i=0; i<size;i++) {
 			if(subVerb.contains(target.get(i).getStem())
-					&& target.get(i).getPos()=='V') {
+					&& (target.get(i).getPos()=='V'||target.get(i).getPatn()==3||target.get(i).getPatn()==8)) {
 				String check = former.get(0).getSource()
 						.substring(former.get(0).getStem().length());
 				if(target.get(i).getStem().equals("보다")) {
 					if(check.equals("어")){
-						target.get(i).setPosType('b');
+						target.get(i).setPosType('b'); //의미가 약한 보조 동사(생략 가능)
 					} else if(check.equals("고")||check.equals("다")||check.equals("다가")) {
-						
+						target.get(i).setPosType('b'); //앞말이 뒷말의 원인(?)
 					} else if(check.endsWith("은가") || check.endsWith("는가")
 							||check.endsWith("나")||check.endsWith("까")) {
-						target.get(i).setPosType('b'); //보조 형용사
+						target.get(i).setPosType('b'); //의도 추측의 의미(보조형용사3번 분류 필요)
 					} 
-				} else if(target.get(i).getStem().equals("가다")
-						||target.get(i).getStem().equals("가지다")) {
+				} else if(target.get(i).getStem().equals("가다")) {
 					if(check.endsWith("어")) {
-						target.get(i).setPosType('b');
+						target.get(i).setPosType('b'); //진행의 의미
+					}
+				} else if(target.get(i).getStem().equals("가지다")) {
+					if(check.endsWith("어")) {
+						target.get(i).setPosType('b'); //생략 가능 보조동사
 					}
 				} else if(target.get(i).getStem().equals("나다")) {
 					if(check.equals("어")||check.equals("고")) {
-						target.get(i).setPosType('b');
+						target.get(i).setPosType('b'); //행동의 종결 의미
 					}
 				} else if(target.get(i).getStem().equals("내다")) {
 					if(check.equals("어")) {
-						target.get(i).setPosType('b');
+						target.get(i).setPosType('b'); //생략가능
 					}
 				} else if(target.get(i).getStem().equals("놓다")) {
 					if(check.equals("어")) {
-						target.get(i).setPosType('b');
+						target.get(i).setPosType('b'); //2번은 생략 가능 1번은 유지의 의미
 					}
 				} else if(target.get(i).getStem().equals("대다")
 						||target.get(i).getStem().equals("두다")
-						||target.get(i).getStem().equals("먹다")
 						||target.get(i).getStem().equals("버리다")
 						||target.get(i).getStem().equals("오다")
 						||target.get(i).getStem().equals("주다")) {
 					if(check.equals("어")||former.get(0).getEomi().endsWith("어")) {
-						target.get(i).setPosType('b');
+						target.get(i).setPosType('b'); //생략가능
 					}
+				} else if(target.get(i).getStem().equals("먹다")) {
+					if(check.equals("어")||former.get(0).getEomi().endsWith("어")) {
+						target.get(i).setPosType('b'); //강조의 의미
 				} else if(target.get(i).getStem().equals("말다")) {
+				}
 					if(check.equals("지")) {
 						target.get(i).setPosType('b'); //부정의 의미
 					} else if(check.equals("고")||check.equals("고야")) {
