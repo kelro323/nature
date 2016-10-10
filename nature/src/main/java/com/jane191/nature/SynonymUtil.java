f6f2780c9310b3c85057faaa83ce337ea1a65029
Seungwoo <Seungwoo@Biz>
2016-10-10 16:10:10 +0900

    SynonymHandler name change

--- nature/src/main/java/com/jane191/nature/SynonymUtil.java
+++ nature/src/main/java/com/jane191/nature/SynonymUtil.java
@@ -3,8 +3,8 @@ package com.jane191.nature;
 import org.apache.lucene.analysis.ko.utils.DictionaryUtil;
 import org.apache.lucene.analysis.ko.utils.SynonymEntry;
 
-public class SynonymHandler {
-	public SynonymOutput synonym(String key) {
+public class SynonymUtil {
+	public static SynonymOutput synonym(String key) {
 		SynonymEntry entry = DictionaryUtil.getSynonym(key);
 		if(entry==null) return null;
 		SynonymOutput syOutput = new SynonymOutput(entry.getCategory(), entry.getDetail(),
